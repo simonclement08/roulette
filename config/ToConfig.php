ef39f3e96e7736c4bf433164611ab5f741bc9b66
prof <profinfodev@gmail.com>
2020-03-20 14:55:30 +0100

    Add config/install.sh

--- config/ToConfig.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('DB_HOST',    'localhost');
-define('DB_NAME',    'ROULETTE');
-define('DB_USER',    'root');
-define('DB_PWD',     'root');
---
+++ config/_config.php
@@ -0,0 +1,6 @@
+<?php
+
+	define('DB_HOST',    'localhost');
+	define('DB_NAME',    'ROULETTE');
+	define('DB_USER',    'root');
+	define('DB_PWD',     'root');
